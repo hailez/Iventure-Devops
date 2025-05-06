efd20058854712c71dc8fac13589aae6479d0793
Hailes Antony <hailesantony@MacBookPro.fritz.box>
2025-05-06 11:01:08 +0200

    Argocd V2

--- configure_overlay.sh
+++ configure_overlay.sh
@@ -1,1 +1,3 @@
+kubectl create ns monitoring 
+kubectl apply --server-side -f "$(pwd)/kube-prometheus/apiextensions.k8s.io_v1_customresourcedefinition_prometheuses.monitoring.coreos.com.yaml"
 kubectl apply -k $(pwd)/overlays/prometheus
